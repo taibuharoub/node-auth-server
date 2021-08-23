efb82c8f87c6d7a401a6b560fca6eb224d6f6c22
Taibu Haroub <haroubtaibu@gmail.com>
2021-08-23 14:29:12 +0300

    Fix test

--- test/add.js
+++ test/add.js
@@ -4,7 +4,7 @@ it("should add numbers correctly", function () {
   const num1 = 2;
   const num2 = 3;
 
-  expect(num1 + num2).to.equal(7);
+  expect(num1 + num2).to.equal(5);
 });
 
 it("should not give a result of 6", function () {
